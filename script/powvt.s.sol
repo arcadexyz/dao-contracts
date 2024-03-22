5049c65d958cf59fcbbdb4fcdf35d6098f9f9489
PowVT <epowlowsky@gmail.com>
2024-03-22 10:13:00 -0400

    add script instructions

--- script/powvt.s.sol
+++ script/powvt.s.sol
@@ -4,6 +4,15 @@ pragma solidity 0.8.20;
 import "forge-std/Script.sol";
 import "../src/ArcadeStakingRewards.sol";
 
+/**
+ * To run this script, you need to set the following environment variables:
+ * - PRIVATE_KEY: the private key of the deployer
+ * - MAINNET_FORK_RPC_URL: the RPC url of the mainnet fork
+ *
+ * To run this script use:
+ * `forge script script/powvt.s.sol:MyScript --fork-url $MAINNET_FORK_RPC_URL --broadcast --legacy -vvvv`
+ * Must use `--legacy` flag to disable EIP1559 tx's, otherwise script with not able to estimate gas and fail.
+ */
 contract MyScript is Script {
     function run() external {
         // constructor arguments
