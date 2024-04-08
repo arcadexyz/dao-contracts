daa5dc6cbb5b5561f113d5b35579aa93b27cb622
Mouzayan Delbourgo <mouzayan@arcade.xyz>
2024-04-08 16:39:20 -0400

    chore(create-pool-mainet): deploy pool on mainnet

--- script/PoolInitiation.s.sol
+++ script/PoolInitiation.s.sol
@@ -18,17 +18,17 @@ contract PoolInitiation is Script {
         address router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
         address arcd = 0xe020B01B6fbD83066aa2e8ee0CCD1eB8d9Cc70bF;
 
-        uint tokenAmount = 334e18; // TODO: Update amounts before Mainnet deploy
+        uint tokenAmount = 334e18;
         uint tokenAmountMin = 1e18;
         uint ethAmountMin = 1e16;
 
         vm.startBroadcast();
 
-        IUniswapV2Router02 uniswapRouter02 = IUniswapV2Router02(router);
+        IUniswapV2Router02 uniswapV2Router02 = IUniswapV2Router02(router);
 
-        IERC20(arcd).approve(address(uniswapRouter02), tokenAmount);
+        IERC20(arcd).approve(address(uniswapV2Router02), tokenAmount);
 
-        uniswapRouter02.addLiquidityETH{value: 100000000000000000}(
+        uniswapV2Router02.addLiquidityETH{value: 100000000000000000}(
             arcd,
             tokenAmount,
             tokenAmountMin,
