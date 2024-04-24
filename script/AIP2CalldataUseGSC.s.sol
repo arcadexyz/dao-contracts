2f79d83a83de6346226285d031fdc151687f5737
PowVT <epowlowsky@gmail.com>
2024-04-24 09:28:54 -0400

    encoding for GSC

--- script/AIP2CalldataUseGSC.s.sol
+++ script/AIP2CalldataUseGSC.s.sol
@@ -12,7 +12,7 @@ import {console2} from "forge-std/console2.sol";
  *      are also hardcoded and may need to be updated.
  *
  * to run:
- * forge script script/AIP2Calldata.s.sol --fork-url <your-mainnet-rpc-url> --block-number 19683254 -vvv
+ * forge script script/AIP2CalldataUseGSC.s.sol --fork-url <your-mainnet-rpc-url> --block-number 19683254 -vvv
  */
 interface ITimelock {
     function callTimestamps(bytes32) external view returns (uint256);
@@ -64,21 +64,26 @@ interface IArcadeCoreVoting {
     function approvedVaults(address) external view returns (bool);
 }
 
-contract AIP2Calldata is Script {
+contract AIP2CalldataUseGSC is Script {
     uint256 public constant DAY_IN_BLOCKS = 7150;
     uint256 public lockDuration = DAY_IN_BLOCKS * 3;
+    uint256 public gscLockDuration = 2165; // blocks
 
     // mainnet governance contracts
     ITimelock public timelock = ITimelock(0x47511465C397875deAb7cf8f008d7922D041fF01);
+
     IArcadeCoreVoting public arcadeCoreVoting = IArcadeCoreVoting(0x54B7235dB74103395dD48A2c3dd993E3b7d39856);
+    IArcadeCoreVoting public gscCoreVoting = IArcadeCoreVoting(0x2b6F11B2A783C928799C4E561dA89cD06894A279);
+
     address public teamVestingVault = 0xae40Af135C060E10b218C617c2d74A370B09C40F;
+    address public gscVotingVault = 0xFd2D1c8809A271e892046A23185423a52A149F62;
 
     // mainnet staking contracts
     address public stakingRewardsAddress = 0x80bDdd56b947c547Ab8964D80E98E42Ff77a5793;
     address public singleSidedStakingAddress = 0x72854FBb44d3dd87109D46a9298AEB0d018740f0;
 
     // mainnet actors
-    address public whale1 = 0xF70f7c0fCD743b2c03b823672A0B02B6a1e1bA20; // 4,463,281 ARCD
+    address public whale1 = 0xF70f7c0fCD743b2c03b823672A0B02B6a1e1bA20; // 4,463,281 ARCD (GSC)
     address public whale2 = 0x6888d7Ef74b081060a0165E336A9d03b809098BE; // 4,463,281 ARCD
 
     function run() external {
@@ -96,52 +101,102 @@ contract AIP2Calldata is Script {
         timelockCalldatas[1] = coreVotingCalldata2;
 
         bytes32 timelockCallHash = keccak256(abi.encode(timelockTargets, timelockCalldatas));
-
         bytes memory timelockCalldata = abi.encodeWithSignature("registerCall(bytes32)", timelockCallHash);
-        console2.log("Target contract to be used in the proposal");
-        console2.logAddress(address(timelock));
-        console2.log("Calldata to be used in proposal");
-        console2.logBytes(timelockCalldata);
+
+        address[] memory votingVaults1 = new address[](1);
+        bytes[] memory extraVaultData1 = new bytes[](1);
+        address[] memory targets1 = new address[](1);
+        bytes[] memory calldatas1 = new bytes[](1);
+
+        targets1[0] = address(timelock);
+        calldatas1[0] = timelockCalldata;
+        bytes memory gscCoreVotingCalldata = abi.encodeWithSignature(
+            "proposal(address[],bytes[],address[],bytes[],uint256,uint8)",
+            votingVaults1,
+            extraVaultData1,
+            targets1,
+            calldatas1,
+            block.number + 1000000,
+            IArcadeCoreVoting.Ballot.YES
+        );
+        console2.log("Target contract to be used in the GSC proposal");
+        console2.logAddress(address(arcadeCoreVoting));
+        console2.log("Calldata to be used in GSC proposal");
+        console2.logBytes(gscCoreVotingCalldata);
 
         /// @notice Execute proposal calldata on mainnet fork
-        address[] memory votingVaults = new address[](1);
-        bytes[] memory extraVaultData = new bytes[](1);
-        address[] memory targets = new address[](1);
-        bytes[] memory calldatas = new bytes[](1);
+        address[] memory votingVaults2 = new address[](1);
+        bytes[] memory extraVaultData2 = new bytes[](1);
+        address[] memory targets2 = new address[](1);
+        bytes[] memory calldatas2 = new bytes[](1);
 
-        votingVaults[0] = teamVestingVault;
-        extraVaultData[0] = bytes("");
-        targets[0] = address(timelock);
-        calldatas[0] = timelockCalldata;
+        votingVaults2[0] = gscVotingVault;
+        extraVaultData2[0] = bytes("");
+        targets2[0] = address(arcadeCoreVoting);
+        calldatas2[0] = gscCoreVotingCalldata;
 
-        // whale account creates the proposal in CoreVoting
+        // whale account creates the proposal in GSC CoreVoting
         vm.prank(whale1);
-        arcadeCoreVoting.proposal(
-            votingVaults, extraVaultData, targets, calldatas, block.number + 1000000, IArcadeCoreVoting.Ballot.YES
+        gscCoreVoting.proposal(
+            votingVaults2, extraVaultData2, targets2, calldatas2, block.number + 1000000, IArcadeCoreVoting.Ballot.YES
         );
 
         // ensure the proposal is created
-        uint128[3] memory propVotes = arcadeCoreVoting.getProposalVotingPower(17);
-        assert(propVotes[0] == 4_463_281 ether);
+        uint128[3] memory propVotes = gscCoreVoting.getProposalVotingPower(0);
+        assert(propVotes[0] == 1);
 
-        // second vote passes the proposal with their vote
+        // fast forward to the end of the voting time
+        vm.roll(block.number + gscLockDuration);
+
+        // execute the proposal
+        //
+        //
+        //
+        //
+        //
+        // NOTE: Needs 3 GSC members to vote in order to pass, there are only 2 currently at this block
+        //
+        //
+        //
+        //
+        //
+        //
+        gscCoreVoting.execute(0, targets2, calldatas2);
+
+        // ensure the proposal is executed
+        uint128[3] memory propVotes3 = gscCoreVoting.getProposalVotingPower(0);
+        assert(propVotes3[0] == 0);
+
+        // ensure the proposal is created
+        uint128[3] memory propVotes4 = arcadeCoreVoting.getProposalVotingPower(17);
+        assert(propVotes4[0] == 0 ether);
+
+        // both whales vote to pass the proposal
         // quorum is 6 million ARCD
+        address[] memory votingVaultsWhale = new address[](1);
+        bytes[] memory extraVaultDataWhale = new bytes[](1);
+
+        votingVaultsWhale[0] = teamVestingVault;
+        extraVaultDataWhale[0] = bytes("");
+        vm.prank(whale1);
+        arcadeCoreVoting.vote(votingVaultsWhale, extraVaultDataWhale, 17, IArcadeCoreVoting.Ballot.YES);
+
         vm.prank(whale2);
-        arcadeCoreVoting.vote(votingVaults, extraVaultData, 17, IArcadeCoreVoting.Ballot.YES);
+        arcadeCoreVoting.vote(votingVaultsWhale, extraVaultDataWhale, 17, IArcadeCoreVoting.Ballot.YES);
 
         // ensure the vote is counted
-        uint128[3] memory propVotes2 = arcadeCoreVoting.getProposalVotingPower(17);
-        assert(propVotes2[0] == 4_463_281 ether + 4_463_281 ether);
+        uint128[3] memory propVotes5 = arcadeCoreVoting.getProposalVotingPower(17);
+        assert(propVotes5[0] == 4_463_281 ether + 4_463_281 ether);
 
         // fast forward to the end of the voting time
         vm.roll(block.number + lockDuration);
 
         // execute the proposal
-        arcadeCoreVoting.execute(17, targets, calldatas);
+        arcadeCoreVoting.execute(17, targets1, calldatas1);
 
         // ensure the proposal is executed
-        uint128[3] memory propVotes3 = arcadeCoreVoting.getProposalVotingPower(17);
-        assert(propVotes3[0] == 0);
+        uint128[3] memory propVotes6 = arcadeCoreVoting.getProposalVotingPower(17);
+        assert(propVotes6[0] == 0);
 
         // ensure the timelock call is executed
         uint256 callTimestamp = timelock.callTimestamps(timelockCallHash);
